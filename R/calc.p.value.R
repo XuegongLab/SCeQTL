ed812c2b86ac796ebf39334e0fa5bc9f40e59966
Xixi-thu <57483791+Xixi-thu@users.noreply.github.com>
2020-04-16 21:10:54 +0800

    Update calc.p.value.R

--- R/calc.p.value.R
+++ R/calc.p.value.R
@@ -26,89 +26,76 @@
 #' pvalue <- cal.pvalue(gene, snp, dist = 'poisson')
 #' pvalue <- cal.pvalue(gene, snp, type = 2)
 cal.pvalue <- function(gene, snp, thread = 8, remove_outlier = TRUE,EM = TRUE, dist = 'negbin', type = 0){
-    removeoutlier <- function(sample.data){
-        sample.data.no.zero = sample.data[sample.data$expression!=0,1]
-        med = median(sample.data.no.zero)
-        Mad = mad(sample.data.no.zero)
-        return(sample.data[sample.data$expression<med+4*Mad,])
+  removeoutlier <- function(sample.data){
+    sample.data.no.zero = sample.data[sample.data$expression!=0,1]
+    med = median(sample.data.no.zero)
+    Mad = mad(sample.data.no.zero)
+    return(sample.data[sample.data$expression<med+4*Mad,])
+  }
+  
+  zeroinfl_model <- function(sample_gene, sample_snp, remove_outlier=TRUE, dist='negbin', EM = TRUE, type = 0){
+    sample.data = data.frame(unlist(sample_gene), unlist(sample_snp))
+    colnames(sample.data) = c('expression','snp')
+    if(remove_outlier)
+      sample.data = removeoutlier(sample.data)
+    m1 <- try(zeroinfl(expression ~ snp, data = sample.data, dist = dist, EM = EM), silent=TRUE)
+    if(class(m1)=="try-error")
+      return(NA)
+    
+    if(type==0){
+      m0 <- try(zeroinfl(expression ~ snp|1, data = sample.data, dist = dist, EM = EM))
+      if(class(m0)=="try-error") return(NA)
+      .df <- 1
     }
-
-    zeroinfl_model <- function(sample_gene, sample_snp, remove_outlier=TRUE, dist='negbin', EM = TRUE, type = 0){
-        sample.data = data.frame(unlist(sample_gene), unlist(sample_snp))
-        colnames(sample.data) = c('expression','snp')
-        if(remove_outlier)
-          sample.data = removeoutlier(sample.data)
-        m1 <- try(zeroinfl(expression ~ snp, data = sample.data, dist = dist, EM = EM), silent=TRUE)
-        if(class(m1)=="try-error")
-          return(NA)
-
-        if(type==0){
-          m0 <- try(zeroinfl(expression ~ snp|1, data = sample.data, dist = dist, EM = EM))
-          if(class(m0)=="try-error"){
-            message("\twarning: no valid set of coefficients has been found\n")
-            return(NA)
-          }
-          .df <- 1
-        }
-        else if(type==1){
-          m0 <- try(zeroinfl(expression ~ 1|snp, data = sample.data, dist = dist, EM = EM))
-          if(class(m0)=="try-error"){
-            message("\twarning: no valid set of coefficients has been found\n")
-            return(NA)
-          }
-          .df <- 1
-        }
-        else{
-          m0 <- try(zeroinfl(expression ~ 1, data = sample.data, dist = dist, EM = EM))
-          if(class(m0)=="try-error"){
-            message("\twarning: no valid set of coefficients has been found\n")
-            return(NA)
-          }
-          .df <- 2
-        }
-        pvalue = try(pchisq(2 * (logLik(m1) - logLik(m0)), df = .df, lower.tail=FALSE))
-        if(class(pvalue)=="try-error"){
-          message("\twarning: no valid set of coefficients has been found\n")
-          return(NA)
-        }
-        else
-          return(pvalue)
+    else if(type==1){
+      m0 <- try(zeroinfl(expression ~ 1|snp, data = sample.data, dist = dist, EM = EM))
+      if(class(m0)=="try-error") return(NA)
+      .df <- 1
     }
-    if (type == 0) 
-        message("Identyfing non-zero part difference...\n")
-    else if (type == 1) 
-        message("Identyfing zero ratio difference...\n")
-    else 
-        message("Identyfing non-zero part or/and zero ratio difference...\n")
-    registerDoParallel(thread)
-    countzero <- rowSums(gene != 0)
-    gene <- gene[countzero > 3,]
-    removed <- labels(countzero)[countzero <= 3]
-    if (length(removed) != 0)
-        for(i in 1:length(removed))
-            message(paste0("Gene: ", removed[i], " was removed because of excess zero.\n"))
-    else 
-        message('No gene was removed because of excess zero.\n')
-    if (nrow(gene) == 0) stop('No gene to be tested.')
-    gene.count = dim(gene)[1]
-    snp.count = dim(snp)[1]
-    pvalue = list()
-    j = 0
-    message("Start calculating p value...\n")
-    for(i in 1:gene.count){
-        message(paste0("calculate pvalue for gene: ", i, "\n"))
-        result = foreach(j=1:snp.count) %dopar% {zeroinfl_model(gene[i,],snp[j,],remove_outlier=remove_outlier,dist=dist,EM=EM,type=type)}
-        pvalue = rbind(pvalue,result)
+    else{
+      m0 <- try(zeroinfl(expression ~ 1, data = sample.data, dist = dist, EM = EM))
+      if(class(m0)=="try-error") return(NA)
+      .df <- 2
     }
-    gene.name = rep(row.names(gene), each = snp.count)
-    if(is.null(row.names(snp))){
-        snp.raw.name = 1:snp.count
-    }else
-        snp.raw.name = row.names(snp)
-    snp.name = list()
-    for(i in 1:gene.count)
-        snp.name = c(snp.name, snp.raw.name)
-    result = data.frame(gene.name, unlist(snp.name), unlist(pvalue))
-    colnames(result) <- c("gene","snp","pvalue")
-    return(result)
+    pvalue = try(pchisq(2 * (logLik(m1) - logLik(m0)), df = .df, lower.tail=FALSE))
+    if(class(pvalue)=="try-error") return(NA)
+    else return(pvalue)
+  }
+  if (type == 0) 
+    message("Identyfing non-zero part difference...\n")
+  else if (type == 1) 
+    message("Identyfing zero ratio difference...\n")
+  else 
+    message("Identyfing non-zero part or/and zero ratio difference...\n")
+  registerDoParallel(thread)
+  countzero <- rowSums(gene != 0)
+  gene <- gene[countzero > 3,]
+  removed <- labels(countzero)[countzero <= 3]
+  if (length(removed) != 0)
+    for(i in 1:length(removed))
+      message(paste0("Gene: ", removed[i], " was removed because of excess zero.\n"))
+  else 
+    message('No gene was removed because of excess zero.\n')
+  if (nrow(gene) == 0) stop('No gene to be tested.')
+  gene.count = dim(gene)[1]
+  snp.count = dim(snp)[1]
+  pvalue = list()
+  j = 0
+  message("Start calculating p value...\n")
+  for(i in 1:gene.count){
+    message(paste0("calculate pvalue for gene: ", i, "\n"))
+    result = foreach(j=1:snp.count) %dopar% {zeroinfl_model(gene[i,],snp[j,],remove_outlier=remove_outlier,dist=dist,EM=EM,type=type)}
+    pvalue = rbind(pvalue,result)
+  }
+  gene.name = rep(row.names(gene), each = snp.count)
+  if(is.null(row.names(snp))){
+    snp.raw.name = 1:snp.count
+  }else
+    snp.raw.name = row.names(snp)
+  snp.name = list()
+  for(i in 1:gene.count)
+    snp.name = c(snp.name, snp.raw.name)
+  result = data.frame(gene.name, unlist(snp.name), unlist(pvalue))
+  colnames(result) <- c("gene","snp","pvalue")
+  return(result)
 }
